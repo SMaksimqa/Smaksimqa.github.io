6ee92417060d6e308abd66d946ec13cd629de4fc
SMaksimqa <liveestar24@gmail.com>
2025-07-17 21:18:06 +0300

    Update test_food.py

--- tests/test_food.py
+++ tests/test_food.py
@@ -1,32 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Мой сайт</title>
-<style>
-  body {
-  colod:black;
-}
-h1 {
-  color: yellow;
-  background-color: black
-}
-  
- </style> 
-  </head>
-<body>
-    <h1>"Простой бутерброд с сыром"</h1>
-    
-    <h2> Что нужно: </h2>
- <p> "Хлеб, сливочное масло, сыр, зелень (по желанию)"</p>
- <h2> Разбейте инструкцию на отдельные шаги, каждый в своём абзаце :
-<ul>
- <li>  "Намажьте хлеб маслом."  </li>
-<li>"Положите сверху ломтик сыра."</li>
-<li>"Добавьте зелень, если хотите."</li>
-</ul>
-"Бутерброд готов!" </h2> 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+def test_start_page():
+  driver = webdriver.Chrome()
+  driver.get("https://smaksimqa.github.io/sandwichs.html")
+  assert driver.find_element(By.TAG_NAME, "h1").text == "Простой бутерброд с сыром"
+  assert driver.find_element(By.TAG_NAME, "h2").text == "Что нужно"
+  assert driver.find_element(By.TAG_NAME, "h2").text == "Как приготовить"
 
-  
-    
-</body>
-</html>
+  try:
+       element = driver.find_element(By.TAG_NAME, "body")
+       page_text = element.text
+
+       assert "Намажьте хлеб маслом." in page_text
+       assert "Положите сверху ломтик сыра." in page_text
+       assert "Добавьте зелень, если хотите." in page_text
+       assert "Бутерброд готов!" in page_text
+   finally:
+       time.sleep(3)
+       driver.quit()
