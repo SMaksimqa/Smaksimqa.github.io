5ba49a5bdbe9979a0e16895fe923044d6c5281e0
SMaksimqa <liveestar24@gmail.com>
2025-07-17 21:32:25 +0300

    Update test_food.py
    Рабочий (новый) вариант

--- tests/test_food.py
+++ tests/test_food.py
@@ -7,15 +7,17 @@ def test_start_page():
   assert driver.find_element(By.TAG_NAME, "h1").text == "Простой бутерброд с сыром"
   assert driver.find_element(By.TAG_NAME, "h2").text == "Что нужно"
   assert driver.find_element(By.TAG_NAME, "h2").text == "Как приготовить"
-
   try:
-       element = driver.find_element(By.TAG_NAME, "body")
-       page_text = element.text
+    element = driver.find_element(By.TAG_NAME, "body")
+    page_text = element.text
+    assert "Намажьте хлеб маслом." in page_text
+    assert "Положите сверху ломтик сыра." in page_text
+    assert "Добавьте зелень, если хотите." in page_text
+    assert "Бутерброд готов!" in page_text
+  finally:
+    time.sleep(3)
+    driver.quit()
 
-       assert "Намажьте хлеб маслом." in page_text
-       assert "Положите сверху ломтик сыра." in page_text
-       assert "Добавьте зелень, если хотите." in page_text
-       assert "Бутерброд готов!" in page_text
-   finally:
-       time.sleep(3)
-       driver.quit()
+
+
+
